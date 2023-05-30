b0eb5c174175058e8525d4a3bbf8f6d7f906bcfb
Rafael Soares <89417905+RafaelHGS@users.noreply.github.com>
2023-05-31 04:08:23 +0845

    API_client

---
+++ java/TesteAPIGestor/api/src/main/java/br/com/gestorfinanceiro/api/Controller/AuthClient.java
@@ -0,0 +1,67 @@
+package br.com.gestorfinanceiro.api.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.gestorfinanceiro.api.Model.Client;
+import br.com.gestorfinanceiro.api.Repo.ClientRepo;
+import br.com.gestorfinanceiro.api.Service.AuthService;
+
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin(origins = "*")
+public class AuthClient {
+    
+    @Autowired
+    private ClientRepo repoAction;
+    
+    @Autowired
+    private AuthService authService;
+
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signUp(@RequestBody Client client){
+        return authService.signup(client);
+    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Client client) {
+        return authService.login(client);
+    }
+
+
+    // @GetMapping("/")
+    // public Iterable<Client> select(){
+    //     return repoAction.findAll();
+    // }
+
+    @GetMapping("/{email}")
+    public Client findUsername(@PathVariable String email){
+        return repoAction.findByEmail(email);
+    }
+
+
+    @PutMapping("/puuuuut")
+    public Client edit(@RequestBody Client client){
+        return repoAction.save(client);
+    }
+
+
+    @DeleteMapping("/aroba/{id}")
+    public void delete(@PathVariable Long id){
+        repoAction.findById(id);
+    }
+
+
+}
---
+++ java/TesteAPIGestor/api/src/main/java/br/com/gestorfinanceiro/api/Model/SystemMessage.java
@@ -0,0 +1,15 @@
+package br.com.gestorfinanceiro.api.Model;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Getter
+@Setter
+public class SystemMessage {
+
+    private String message;
+    
+}
