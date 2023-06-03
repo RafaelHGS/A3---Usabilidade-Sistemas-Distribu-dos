10e660a1cd5d8ba892ed1f4e4cf0c0f78eeb138d
Diego <martinezdiego906@gmail.com>
2023-06-03 00:27:43 -0300

    parse do valor recebido nos inputs para float tentativa de resolver as promises

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -3,147 +3,149 @@ import { AlertController, ToastController, PopoverController } from '@ionic/angu
 import { TkService } from '../services/tk.service';
 import { Router } from '@angular/router';
 
-
-
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss'],
 })
 
 export class HomePage {
+	type: string = "ganho";
 
-  type: string = "ganho";
+	constructor(
+		private alertController: AlertController,
+		public tkService: TkService,
+		public toastController: ToastController,
+		public popoverController: PopoverController,
+		public router: Router,
+	) { }
 
-  constructor(private alertController: AlertController,
-    public tkService: TkService,
-    public toastController: ToastController,
-    public popoverController: PopoverController,
-    public router: Router,) { }
+	ngOnInit() {
+		this.tkService.setGastoGanhos().catch(error => console.error(error)); // Eu nao sei se isso resolve alguma coisa
+		// if (this.tkService.getGastoGanho() === null){
+		//   this.tkService.getGastoGanhoFromStorage();
+		// }
+	}
 
-  ngOnInit() {
-    this.tkService.setGastoGanhos();
-    // if (this.tkService.getGastoGanho() == null){
-    //   this.tkService.getGastoGanhoFromStorage();
-    // }
+	logout() {
+		this.popoverController.dismiss().catch(error => console.error(error));
+		this.router.navigate(["login"]).catch(error => console.error(error));
+	}
 
-  }
+	profile() {
+		this.popoverController.dismiss().catch(error => console.error(error));
+		this.router.navigate(["profile"]).catch(error => console.error(error));
+	}
 
+	async presentAlertPromptAdicionar() {
+		const alert = await this.alertController.create({
+			header: "Novo Gasto/Ganho",
+			inputs: [
+				{
+					name: "gastoGanho",
+					type: "text",
+					placeholder: "Gasto/Ganho"
+				},
+				{
+					name: "valor",
+					type: "number",
+				}
+			],
+			buttons: [
+				{
+					text: "cancelar",
+					role: "cancel",
+					handler: () => {
+						console.log("Confirm Cancel");
+					}
+				},
+				{
+					text: "Ok",
+					handler: (dadosAlert) => {
+						const valor = parseFloat(dadosAlert.valor); // E preciso parsear o float do valor recebido no input(?)
+						if (dadosAlert.gastoGanho != "" && !isNaN(valor))
+							this.tkService.adicionarGastoGanho(dadosAlert.gastoGanho, valor)
+						else {
+							this.presentToast().catch(error => console.error(error));
+							this.presentAlertPromptAdicionar().catch(error => console.error(error));
+						}
+					}
+				}
+			]
+		})
+		await alert.present();
+	}
 
-  logout() {
-    this.popoverController.dismiss();
-    this.router.navigate(["login"])
-  }
+	async presentAlertPromptLimpar(index: number, valor: number) {
+		const alert = await this.alertController.create({
+			header: "Exclusão !!!",
+			message: 'Deseja Excluir essa Entrada/Saída ?',
+			buttons: [
+				{
+					text: "cancelar",
+					role: "cancel",
+				}, {
+					text: "Excluir",
+					handler: () => this.tkService.limparGastoGanho(index, valor * -1)
+				}
+			]
+		})
+		await alert.present();
+	}
 
-  profile() {
-    this.popoverController.dismiss();
-    this.router.navigate(["profile"])
-  }
+	async presentAlertPromptAtualizar(index: number, gastoGanho: any) {
+		const alert = await this.alertController.create({
+			header: "Alterar Campo",
+			inputs: [
+				{
+					name: "gastoGanho",
+					type: "text",
+					placeholder: "Gasto/Ganho",
+					value: gastoGanho.nome
+				},
+				{
+					name: "valor",
+					type: "number",
+					value: gastoGanho.valor
+				}
+			],
+			buttons: [
+				{
+					text: "cancelar",
+					role: "cancel",
+					handler: () => {
+						console.log("Confirm Cancel");
+					}
+				},
+				{
+					text: "Salvar",
+					handler: (dadosAlert) => {
+						this.tkService.setSaldo(+gastoGanho.valor * -1);
 
-  async presentAlertPromptAdicionar() {
-    const alert = await this.alertController.create({
-      header: "Novo Gasto/Ganho",
-      inputs: [
-        {
-          name: "gastoGanho",
-          type: "text",
-          placeholder: "Gasto/Ganho"
-        },
-        {
-          name: "valor",
-          type: "number",
-        }
-      ],
-      buttons: [
-        {
-          text: "cancelar",
-          role: "cancel",
-          handler: () => {
-            console.log("Confirm Cancel");
-          }
-        }, {
-          text: "Ok",
-          handler: (dadosAlert) => {
-            if (dadosAlert.gastoGanho != "" && dadosAlert.valor != "")
-              this.tkService.adicionarGastoGanho(dadosAlert.gastoGanho, dadosAlert.valor)
-            else {
-              this.presentToast();
-              this.presentAlertPromptAdicionar();
-            }
-          }
-        }
-      ]
-    })
-    await alert.present();
-  }
+						const valor = parseFloat(dadosAlert.valor); // E preciso parsear o float do valor recebido no input(?)
+						if (dadosAlert.gastoGanho != "" && !isNaN(valor)) {
+							this.tkService.atualizarGastoGanho(index, dadosAlert.gastoGanho, valor)
+						} else {
+							this.presentToast().catch(error => console.error(error));
+							// mesma funcao chamada no if?
+							// this.tkService.atualizarGastoGanho(index, dadosAlert.gastoGanho, dadosAlert.valor)
+							this.presentAlertPromptAtualizar(index, gastoGanho).catch(error => console.error(error));
+						}
+					}
+				}
+			]
+		})
+		await alert.present();
+	}
 
+	async presentToast() {
+		const toast = await this.toastController.create({
+			message: "Preencha os campos!",
+			duration: 2500
+		});
 
-  async presentAlertPromptLimpar(index: number, valor: Number) {
-    const alert = await this.alertController.create({
-      header: "Exclusão !!!",
-      message: 'Deseja Excluir essa Entrada/Saída ?',
-      buttons: [
-        {
-          text: "cancelar",
-          role: "cancel",
-        }, {
-          text: "Excluir",
-          handler: () => this.tkService.limparGastoGanho(index, Number(valor) * -1)
-        }
-      ]
-    })
-    await alert.present();
-  }
-
-
-  async presentAlertPromptAtualizar(index: number, gastoGanho: any) {
-    const alert = await this.alertController.create({
-      header: "Alterar Campo",
-      inputs: [
-        {
-          name: "gastoGanho",
-          type: "text",
-          placeholder: "Gasto/Ganho",
-          value: gastoGanho.nome
-        },
-        {
-          name: "valor",
-          type: "number",
-          value: gastoGanho.valor
-        }
-      ],
-      buttons: [
-        {
-          text: "cancelar",
-          role: "cancel",
-          handler: () => {
-            console.log("Confirm Cancel");
-          }
-        },
-        {
-          text: "Salvar",
-          handler: (dadosAlert) => {
-            this.tkService.setSaldo(Number(gastoGanho.valor) * -1);
-            if (dadosAlert.gastoGanho != "" && dadosAlert.valor != "") {
-              this.tkService.atualizarGastoGanho(index, dadosAlert.gastoGanho, dadosAlert.valor)
-            } else {
-              this.presentToast();
-              this.tkService.atualizarGastoGanho(index, dadosAlert.gastoGanho, dadosAlert.valor)
-            }
-          }
-        }
-      ]
-    })
-    await alert.present();
-  }
-
-
-  async presentToast() {
-    const toast = await this.toastController.create({
-      message: "Preencha os campos!",
-      duration: 2500
-    });
-    toast.present();
-  }
-
+		/* By awaiting the toast.present() promise, you ensure that the promise is
+		resolved or rejected before continuing the execution. -ChatGPT */
+		await toast.present();
+	}
 }
